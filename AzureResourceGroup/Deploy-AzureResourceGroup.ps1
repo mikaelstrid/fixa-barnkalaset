f8f3db77dad6de3563d40f70682b80737f27c7fa
Mikael Strid <mikael.strid@outlook.com>
2017-05-25 03:17:11 +0200

    Deployment-instruktioner.

--- AzureResourceGroup/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroup/Deploy-AzureResourceGroup.ps1
@@ -4,7 +4,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'fixa-barnkalaset',
+    [string] $ResourceGroupName = 'group-fbk-test',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
---
+++ DevOps/Manual deployment steps.md
@@ -0,0 +1,8 @@
+*Manuella steg vid deployment av Azure-projektet*
+
+0. Ta bort befintlig resursgrupp.
+1. Kör Deploy av projektet.
+2. Lägg till appsettings för Facebook-inloggningen.
+3. Konfigurera byggservern att göra en release till den nya resursgruppen.
+4. Gör en release.
+5. 

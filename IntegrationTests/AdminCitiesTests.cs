f743fbcc53fb897ce65d93a1c05bc7ba840b731f
Mikael Strid <mikael.strid@outlook.com>
2017-06-26 12:10:53 +0200

    Refaktorerade integrationstestet med autentisering.

--- IntegrationTests/AdminCitiesTests.cs
+++ IntegrationTests/AdminCitiesTests.cs
@@ -40,23 +40,17 @@ namespace IntegrationTests
         public async Task CreateCity_GivenValidModel_ShouldWriteEventToDatabase()
         {
             // ARRANGE
-            var loginRequestMessage = PostRequestHelper.Create(
-                "/konto/logga-in",
-                new Dictionary<string, string>
-            {
-                {"Email", "test@test.com"},
-                {"Password", "B1pdsosp!"}
-            });
-            var loginReponse = await _client.SendAsync(loginRequestMessage);
-            var authenticationToken = CookiesHelper.ExtractCookiesFromResponse(loginReponse)[IdentityCookieName];
+            var identityToken = await LoginAndGetIdentityToken("test@test.com", "B1pdsosp!");
 
-            var getRequest = GetRequestHelper.CreateWithCookiesFromResponse("/admin/stader/skapa", loginReponse);
+            var getRequest = CookiesHelper.PutCookiesOnRequest(
+                GetRequestHelper.Create("/admin/stader/skapa"),
+                CreateCookiesDictionary(IdentityCookieName, identityToken));
             var getResponse = await _client.SendAsync(getRequest);
             getResponse.EnsureSuccessStatusCode();
             
             var antiForgeryToken = await AntiForgeryHelper.ExtractAntiForgeryToken(getResponse);
 
-            var formPostBodyData = new Dictionary<string, string>
+            var postRequestBody = new Dictionary<string, string>
             {
                 {"__RequestVerificationToken", antiForgeryToken},
                 {"Name", "Halmstad"},
@@ -64,17 +58,35 @@ namespace IntegrationTests
                 {"Latitude", "19,5"},
                 {"Longitude", "58,7"}
             };
-
-            var postMessage = CookiesHelper.PutCookiesOnRequest(
-                PostRequestHelper.CreateWithCookiesFromResponse("/admin/stader/skapa", formPostBodyData, getResponse),
-                new Dictionary<string, string> { { IdentityCookieName, authenticationToken } });
+            var postRequest = CookiesHelper.PutCookiesOnRequest(
+                PostRequestHelper.CreateWithCookiesFromResponse("/admin/stader/skapa", postRequestBody, getResponse),
+                CreateCookiesDictionary(IdentityCookieName, identityToken));
             
             // ACT
-            var response = await _client.SendAsync(postMessage);
+            var response = await _client.SendAsync(postRequest);
 
             // ASSERT
             response.StatusCode.Should().Be(HttpStatusCode.Redirect);
             _fixture.EventSourcingDbContext.Events.Count().Should().Be(1);
+        }
+
+        private async Task<string> LoginAndGetIdentityToken(string email, string password)
+        {
+            var loginRequestMessage = PostRequestHelper.Create(
+                "/konto/logga-in",
+                new Dictionary<string, string>
+                {
+                    {"Email", email},
+                    {"Password", password}
+                });
+            var loginReponse = await _client.SendAsync(loginRequestMessage);
+            var identityToken = CookiesHelper.ExtractCookiesFromResponse(loginReponse)[IdentityCookieName];
+            return identityToken;
+        }
+
+        private static Dictionary<string, string> CreateCookiesDictionary(string cookieName, string cookieValue)
+        {
+            return new Dictionary<string, string> { { cookieName, cookieValue } };
         }
     }
 }

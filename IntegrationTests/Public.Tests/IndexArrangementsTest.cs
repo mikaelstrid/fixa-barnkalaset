0e7ede53b89d9f7eaa02f041c6dc2ccc5d856e4c
Mikael Strid <mikael.strid@outlook.com>
2017-07-07 14:52:16 +0200

    Integrationstest för /arrangemang/halmstad.

---
+++ IntegrationTests/Public.Tests/IndexArrangementsTest.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IntegrationTests.Utilities;
+using Pixel.FixaBarnkalaset.Core;
+using Pixel.FixaBarnkalaset.Web;
+using UnitTests.Utilities.TestDataExtensions;
+using Xunit;
+
+namespace IntegrationTests.Public.Tests
+{
+    // https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/testing
+    public class IndexArrangementsTest : IntegrationTestBase
+    {
+        public IndexArrangementsTest(TestFixture<Startup> fixture) : base(fixture) { }
+
+        [Fact]
+        public async Task Index_GivenCityWithTwoArrangements_ShouldReturnResponseWithCityName_AndTwoArrangements()
+        {
+            // ARRANGE
+            var halmstad = new City().Halmstad();
+            PopulateDatabaseWithCities(halmstad);
+            var busfabriken = halmstad.Busfabriken();
+            var laserdome = halmstad.Laserdome();
+            PopulateDatabaseWithArrangements(busfabriken, laserdome);
+
+            // ACT
+            var response = await Client.GetAsync($"/arrangemang/{halmstad.Slug}");
+
+            // ASSERT
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Regex.IsMatch(responseString, $"<h1.*>{halmstad.Name}</h1>").Should().BeTrue();
+            responseString.Should().Contain(busfabriken.Name);
+            responseString.Should().Contain(laserdome.Name);
+        }
+    }
+}
---
+++ IntegrationTests/Public.Tests/IntegrationTestBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Net.Http;
+using IntegrationTests.Utilities;
+using Pixel.FixaBarnkalaset.Core;
+using Pixel.FixaBarnkalaset.Web;
+using Xunit;
+
+namespace IntegrationTests.Public.Tests
+{
+    public abstract class IntegrationTestBase : IClassFixture<TestFixture<Startup>>
+    {
+        protected readonly CultureInfo SwedishCultureInfo = new CultureInfo("sv-SE");
+
+        protected readonly TestFixture<Startup> Fixture;
+        protected readonly HttpClient Client;
+
+        protected IntegrationTestBase(TestFixture<Startup> fixture)
+        {
+            Fixture = fixture;
+            Client = fixture.Client;
+        }
+
+        protected void PopulateDatabaseWithCities( params City[] cities)
+        {
+            Fixture.MyDataDbContext.Cities.AddRange(cities);
+            Fixture.MyDataDbContext.SaveChanges();
+        }
+
+        protected void PopulateDatabaseWithArrangements(params Arrangement[] arrangements)
+        {
+            Fixture.MyDataDbContext.Arrangements.AddRange(arrangements);
+            Fixture.MyDataDbContext.SaveChanges();
+        }
+    }
+}

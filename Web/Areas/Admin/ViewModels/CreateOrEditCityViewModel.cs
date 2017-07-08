5fdc949539b864aa4c6154d7fcda7428a038cc85
Mikael Strid <mikael.strid@outlook.com>
2017-07-08 16:59:20 +0200

    Lade till validering i skapa/ändra stad.

--- Web/Areas/Admin/ViewModels/CreateOrEditCityViewModel.cs
+++ Web/Areas/Admin/ViewModels/CreateOrEditCityViewModel.cs
@@ -4,22 +4,22 @@ namespace Pixel.FixaBarnkalaset.Web.Areas.Admin.ViewModels
 {
     public class CreateOrEditCityViewModel
     {
-        [Required(AllowEmptyStrings = false)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fältet är obligatoriskt")]
         [Display(Name = "Namn")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fältet är obligatoriskt")]
         [RegularExpression("^[abcdefghijklmnopqrstuvwxyz0123456789-]*$")]
         [Display(Name = "Slug")]
         public string Slug { get; set; }
 
-        [Required]
-        [Range(-90.0, 90.0)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fältet är obligatoriskt")]
+        [Range(-90.0, 90.0, ErrorMessage = "Värdet ska vara mellan -90,0 och 90,0")]
         [Display(Name = "Latitud")]
         public double Latitude { get; set; }
 
-        [Required]
-        [Range(-180.0, 180.0)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fältet är obligatoriskt")]
+        [Range(-180.0, 180.0, ErrorMessage = "Värdet ska vara mellan -180,0 och 180,0")]
         [Display(Name = "Longitud")]
         public double Longitude { get; set; }
     }

87c760733c7e5671b602ac9b904dd8ed1f6bb6ea
Mikael Strid <mikael.strid@outlook.com>
2017-07-11 08:38:17 +0200

    Fixade ett integrationstest som fallerade efter omdesign.

--- IntegrationTests/Public.Tests/DetailsArrangementsTest.cs
+++ IntegrationTests/Public.Tests/DetailsArrangementsTest.cs
@@ -43,7 +43,6 @@ namespace IntegrationTests.Public.Tests
             // ASSERT
             //response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
-            Regex.IsMatch(responseString, $"<h2.*>.*{halmstad.Name}.*</h2>").Should().BeTrue();
             Regex.IsMatch(responseString, $"<h1.*>{busfabriken.Name}</h1>").Should().BeTrue();
             responseString.Should().Contain(busfabriken.Description);
         }

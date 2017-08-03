2e73b4710598e4df70baac81e5e94425f5a0a362
Mikael Strid <mikael.strid@outlook.com>
2017-08-03 20:53:44 +0200

    Undersidor för inbjudningskort och O.S.A.

---
+++ Web/Controllers/InvitationCardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pixel.FixaBarnkalaset.Web.Controllers
+{
+    [Route("inbjudningskort")]
+    public class InvitationCardsController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Inbjudningskort | Fixa barnkalaset";
+            ViewData["Description"] = "Vi hjälper dig att designa, trycka och skicka inbjudningskorten.";
+            return View();
+        }
+    }
+}
---
+++ Web/Controllers/RsvpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pixel.FixaBarnkalaset.Web.Controllers
+{
+    [Route("osa")]
+    public class RsvpController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "O.S.A. | Fixa barnkalaset";
+            ViewData["Description"] = "Vi hjälper dig att hålla reda på vilka som kommer och inte kommer.";
+            return View();
+        }
+    }
+}

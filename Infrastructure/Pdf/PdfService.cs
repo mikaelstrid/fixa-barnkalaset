c3a69a7e62b30c175d0c4f5b86c49b6282ba0dff
Mikael Strid <mikael.strid@outlook.com>
2017-12-09 16:44:38 +0100

    Initiala tester med Syncfusion's PDF-motor.

---
+++ Infrastructure/Pdf/PdfService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Syncfusion.Pdf.Parsing;
+
+namespace Pixel.FixaBarnkalaset.Infrastructure.Pdf
+{
+    public class PdfService
+    {
+        public static void FillFormFields()
+        {
+            using (var stream = new FileStream(@"c:\temp\invitation-card-template-1.pdf", FileMode.Open))
+            {
+                var loadedDocument = new PdfLoadedDocument(stream);
+                var loadedForm = loadedDocument.Form;
+
+                foreach (var field in loadedForm.Fields)
+                {
+                    (field as PdfLoadedTextBoxField).Text = (DateTime.Now.Ticks % 10000).ToString();
+                }
+
+                using (var stream2 = new FileStream(@"c:\temp\invitation-card-template-2.pdf", FileMode.OpenOrCreate))
+                {
+                    loadedDocument.Save(stream2);
+                }
+            }
+            
+            ////Get the loaded text box field and fill it.
+            //PdfLoadedTextBoxField loadedTextBoxField = loadedForm.Fields[0] as PdfLoadedTextBoxField;
+            //loadedTextBoxField.Text = "First Name";
+
+            ////Save the modified document.
+            //loadedDocument.Save("sample.pdf");
+
+            ////Close the document
+            //loadedDocument.Close(true);
+        }
+    }
+}

f4aa2ad63c9a55293c0c1e86d3dfbb18bf5010df
Unknown <mikael@pixeldigitalbyra.se>
2018-05-12 15:33:07 +0200

    Interim commit.

---
+++ Core/Interfaces/IPdfService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pixel.FixaBarnkalaset.Core.Interfaces
+{
+    public interface IPdfService
+    {
+        [Obsolete]
+        Stream GetInvitationCardsReviewStream(string reviewTemplateUrl, IEnumerable<Invitation> partyInvitations);
+
+        byte[] GenerateInvitations(byte[] template, int instancesInTemplate, IEnumerable<Invitation> invitations);
+    }
+}
